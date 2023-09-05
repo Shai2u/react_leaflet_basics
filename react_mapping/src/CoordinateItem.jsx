14aee30021f2350ce9d780f0276a573809f83509
Shai Sussman <shai.su@kaplanopensource.co.il>
2023-09-05 17:30:00 +0300

    Rename component

--- react_mapping/src/CoordinateItem.jsx
+++ react_mapping/src/CoordinateItem.jsx
@@ -5,7 +5,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from "@mui/icons-material/Delete";
-export default function TodoItem({todo, remove, toggle}){
+export default function CoordinateItem({todo, remove, toggle}){
     
     const labelId = `checkbox-list-label-${todo.id}`;
     const removeTodo = () =>{
