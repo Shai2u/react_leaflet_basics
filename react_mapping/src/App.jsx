b3d5e57d16cca22144e7c8c0050d29144bd1acba
Shai Sussman <shai.su@kaplanopensource.co.il>
2023-09-05 16:31:57 +0300

    Add react map libre example

---
+++ react_mapping/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './navbar';
+import Map from './map';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    <Map/>
+    </div>
+  );
+}
+
+export default App;
