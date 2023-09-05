8efa7bd2038f2086c2542988373050be565d33bd
Shai Sussman <shai.su@kaplanopensource.co.il>
2023-09-05 17:07:02 +0300

    Add side pane

--- react_mapping/src/App.jsx
+++ react_mapping/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import TitleNav from './TitleNav';
+import LayerPane from './LayerPane'
 import Map from './map';
 
 function App() {
   return (
     <div className="App">
     <TitleNav init_title={"What a map!"}/>
+    <LayerPane/>
     <Map init_lat={32.78} init_lng={35.01} init_zoom={16}/>
     </div>
   );
---
+++ react_mapping/src/LayerPane.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './LayerPane.css';
+
+export default function LayerPane(){
+  return (
+    <div className="pane">
+    </div>
+  );
+}
